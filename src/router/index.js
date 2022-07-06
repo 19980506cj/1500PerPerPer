9ee46ffaf517dc25e4bcb9c6352b799184d9b026
hengqu4 <529914423@qq.com>
2022-07-06 21:02:43 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,36 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
-import routes from './routes'
 import store from '../store'
 import setPageTitle from '@/utils/set-page-title.js'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes:[
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('@/views/home'),
+      meta: {
+        title: '主页'
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/login'),
+      meta: {
+        title: '登陆'
+      }
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('@/views/about'),
+      meta: {
+        title: '关于',
+        noCache: true
+      }
+    }
+  ]
 })
 
 router.beforeEach((to, from, next) => {
@@ -17,3 +42,20 @@ router.beforeEach((to, from, next) => {
 })
 
 export default router
+
+// import { createRouter } from 'vue-router'
+// import Login from '../views/login/index' // 导入我们编写的vue组件
+
+// // Vue.use(Router)
+
+// const router = createRouter({
+//   routes: [
+//     // 配置路由地址
+//     {
+//       path: '/',
+//       name: 'Login',
+//       component: Login
+//     }
+//   ]
+// })
+// export default router
