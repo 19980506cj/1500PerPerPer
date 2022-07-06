8bd918fc06dd105b6b9d82f3cfa63d0c85605640
hengqu4 <529914423@qq.com>
2022-07-06 22:12:38 +0800

    animation login

---
+++ src/views/login/animation.vue
@@ -0,0 +1,52 @@
+<template>
+   <div class="register" :style="{width: fullWidth+'px', height:fullHeight+'px'}">
+    <div class="img_box" :style="{width: fullWidth+'px'}">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    //保存数据，实现动态渲染
+    //注意：这里没有带‘px’单位，在上面添加动态样式时，需要加上px单位！！！
+      fullWidth: document.documentElement.clientWidth,
+      fullHeight: document.documentElement.clientHeight
+
+    }
+  },
+  methods: {
+  //定义数据获取方法
+    handleResize () {
+      this.fullWidth = document.documentElement.clientWidth
+      this.fullHeight = document.documentElement.clientHeight
+    }
+  },
+  created () {
+    window.addEventListener('resize', this.handleResize)
+  },
+  beforeUnmount () {
+    window.removeEventListener('resize', this.handleResize)
+  }
+}
+</script>
+
+<style style="less" scoped>
+.register {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+  .img_box {
+    position:absolute;
+    background-image:url("~@/assets/login_bg.png");
+    background-size:cover;
+    background-repeat:no-repeat;
+    background-position:center center;
+    width:100%;
+    height:100%;
+  }
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,60 @@
 <template>
-  <div>
-    Login
+  <div class="page-container">
+    <div class="img_box">
+      <div class="page-bottom">
+        <input v-model="phoneValue"
+               @input="handleInput(1)"
+               type="number"
+               @blur="animate()"
+               placeholder="请输入手机号"
+               max="11">
+
+        <!-- <button></button> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Field } from 'vant';
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      phoneValue: ""
+    }
+  },
+  methods: {
+    animate () {
+      var re = /^1\d{10}$/;
+      let str = this.phoneValue;
+      if (re.test(str)) {
+        alert('成功')
+      } else {
+        alert('抱歉手机号不合法')
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style style="less" scoped>
+.page-container {
+  width: 100%;
+  height: 100%;
+}
+.img_box {
+  width: 100%;
+  height: 100%;
+  background-image: url('~@/assets/login_bg.png');
+  background-size: cover;
+  position: absolute;
+  z-index: -1;
+  background-repeat: no-repeat;
+}
+.page-bottom {
+  width: 100%;
+  position: absolute;
+  bottom: 50px;
+}
 </style>
